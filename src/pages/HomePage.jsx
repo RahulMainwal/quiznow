9b1e81cd3118395db39c3f4f17dc9ba4966efdfc
Rahul Mainwal <mahatmaji360@gmail.com>
2023-03-19 02:05:43 +0530

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import MiddleHome from "../components/MiddleHome";
-import Permission from "../components/Permission";
+// import Permission from "../components/Permission";
 
 export default function HomePage() {
-    const [showPermissionAlert, setShowPermissionAlert] = useState(false);
-    const permission = JSON.parse(window.localStorage.getItem("permission", JSON.stringify(false)));
-    useEffect(() => {
-        if(permission === null || permission === false){
-            setShowPermissionAlert(true)
-        }
-    }, [permission])
+    // const [showPermissionAlert, setShowPermissionAlert] = useState(false);
+    // const permission = JSON.parse(window.localStorage.getItem("permission", JSON.stringify(false)));
+    // useEffect(() => {
+    //     if(permission === null || permission === false){
+    //         setShowPermissionAlert(true)
+    //     }
+    // }, [permission])
     return (
         <>
         <MiddleHome />
-        {
+        {/* {
             showPermissionAlert
             &&
             <Permission />
-        }
+        } */}
         </>
     )
 }
