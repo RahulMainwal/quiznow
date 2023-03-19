74e2bc38038fb26987e8cbbda538f2ba7724b07d
Rahul Mainwal <mahatmaji360@gmail.com>
2023-03-19 13:16:25 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -1,191 +1,191 @@
 const dummyArr = [{
     "id": "qz1",
-    "title": "Amazon quiz",
+    "title": "English quiz",
     "minutes": 1,
     "imgBanner": "https://thumbs.dreamstime.com/b/quiz-vector-logo-isolate-white-questionnaire-icon-poll-sign-flat-bubble-speech-symbols-concept-social-communication-chatting-223580554.jpg",
     "quiz": [
         {
             "id": "da1",
-            "question": "The Amazon Basics 345L 2 Star Frost-free refrigerator is a _________ door refrigerator.",
-            "answer": "Double",
-            "otpion": ["Single", "Double", "Triple", "Quadruple"]
+            "question": "I have to _____ reading the book by today.",
+            "answer": "complete",
+            "otpion": ["completing", "complete", "will complete", "have completed"]
         },
         {
             "id": "da2",
-            "question": "Which of these features are available in the Amazon Basics 345L 2 Star Frost Free Double Door refrigerator?",
-            "answer": "All of them",
-            "otpion": ["Comfortable", "Affordable", "Useful", "All of them"]
+            "question": "These statistics ____________________ ( not -revise ) since 1997. These statistics were not revised since 1997.",
+            "answer": "These statistics have not been revised since 1997.",
+            "otpion": ["These statistics are not revised since 1997.", "These statistics were not revised since 1997.", "These statistics have not been revised since 1997.", "These statistics has not been revised since 1997."]
         },
         {
             "id": "da3",
-            "question": "The vegetable crisper in the Amazon Basics 345L 2 Star Frost-free Double door refrigerator comes with which of these features?",
-            "answer": "All of these",
-            "otpion": ["Good Design", "Portable", "Variety", "All of these"]
+            "question": "Mrs Tan did not punish his son because he told the truth about the broken vase.",
+            "answer": "Let him off the hook",
+            "otpion": ["Let him off the hook", "Be in hot water", "Feeling blue", "Put my foot in my mouth"]
         },
         {
             "id": "da4",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator has an energy consumption of what?",
-            "answer": "207 kWH/Year",
-            "otpion": ["412 KWH/Year", "120 KWH/Year", "420 KWH/Year", "207 KWH/Year"]
+            "question": "I have my brother _____. my car right now.",
+            "answer": "Repair",
+            "otpion": ["To repair.", "Repairs.", "To repairing", "Repair"]
         },
         {
             "id": "da5",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator comes with how many toughened glass shelves?",
-            "answer": "5",
-            "otpion": ["1", "8", "5", "4"]
+            "question": "What is the name of the river _____ goes through the town?",
+            "answer": "which",
+            "otpion": ["whose", "which", "whom", "who"]
         }
     ]
 },
 
 {
     "id": "qz2",
-    "title": "Amazon quiz",
+    "title": "World GS quiz",
     "minutes": 1,
     "imgBanner": "https://thumbs.dreamstime.com/b/megaphone-banner-quiz-time-vector-stock-illustration-229655834.jpg",
     "quiz": [
         {
             "id": "da1",
-            "question": "The Amazon Basics 345L 2 Star Frost-free refrigerator is a _________ door refrigerator.",
-            "answer": "Double",
-            "otpion": ["Single", "Double", "Triple", "Quadruple"]
+            "question": "What country has the highest life expectancy?.",
+            "answer": "Hong Kong",
+            "otpion": ["Japan", "Italy", "Paris", "Hong Kong"]
         },
         {
             "id": "da2",
-            "question": "Which of these features are available in the Amazon Basics 345L 2 Star Frost Free Double Door refrigerator?",
-            "answer": "All of them",
-            "otpion": ["Comfortable", "Affordable", "Useful", "All of them"]
+            "question": "What is the most common surname in the United States?",
+            "answer": "Smith",
+            "otpion": ["Smith", "Swiper", "Swifter", "Sparsh"]
         },
         {
             "id": "da3",
-            "question": "The vegetable crisper in the Amazon Basics 345L 2 Star Frost-free Double door refrigerator comes with which of these features?",
-            "answer": "All of these",
-            "otpion": ["Good Design", "Portable", "Variety", "All of these"]
+            "question": "What year was the United Nations established?",
+            "answer": "1945",
+            "otpion": ["1941", "1943", "1945", "1947"]
         },
         {
             "id": "da4",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator has an energy consumption of what?",
-            "answer": "207 KWH/Year",
-            "otpion": ["412 KWH/Year", "120 KWH/Year", "420 KWH/Year", "207 KWH/Year"]
+            "question": "How many minutes are in a full week?",
+            "answer": "10,080",
+            "otpion": ["10,079", "10,080", "10,081", "10,082"]
         },
         {
             "id": "da5",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator comes with how many toughened glass shelves?",
-            "answer": "5",
-            "otpion": ["1", "8", "5", "4"]
+            "question": "Who discovered that the earth revolves around the sun?",
+            "answer": "Nicolaus Copernicus",
+            "otpion": ["Royal Prussia", "Catholic canon", "Astronomer", "Nicolaus Copernicus"]
         }
     ]
 },
 {
     "id": "qz3",
-    "title": "Amazon quiz",
+    "title": "History quiz",
     "minutes": 1,
     "imgBanner": "https://th.bing.com/th/id/OIP.bKMTvkxoTMW7rpOg-lRRZwHaH2?pid=ImgDet&rs=1",
     "quiz": [
         {
             "id": "da1",
-            "question": "The Amazon Basics 345L 2 Star Frost-free refrigerator is a _________ door refrigerator.",
-            "answer": "Double",
-            "otpion": ["Single", "Double", "Triple", "Quadruple"]
+            "question": "Who was the founder of the Kanva dynasty?",
+            "answer": "Vassudeva",
+            "otpion": ["Suserman", "Vishnusarma", "Vassudeva", "Vasumitra"]
         },
         {
             "id": "da2",
-            "question": "Which of these features are available in the Amazon Basics 345L 2 Star Frost Free Double Door refrigerator?",
-            "answer": "All of them",
-            "otpion": ["Comfortable", "Affordable", "Useful", "All of them"]
+            "question": "Who started the Saka Era and when?",
+            "answer": "Kanishka in 78 AD",
+            "otpion": ["Kadphises in 58 BC", "Rudradaman-I in 78 AD", "Vikramaditya in 58 BC", "Kanishka in 78 AD"]
         },
         {
             "id": "da3",
-            "question": "The vegetable crisper in the Amazon Basics 345L 2 Star Frost-free Double door refrigerator comes with which of these features?",
-            "answer": "All of these",
-            "otpion": ["Good Design", "Portable", "Variety", "All of these"]
+            "question": "In which of the following sects was Bindusara interested?",
+            "answer": "Ajivakas",
+            "otpion": ["Buddhism", "Ajivakas", "Jainism", "Lokayata"]
         },
         {
             "id": "da4",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator has an energy consumption of what?",
-            "answer": "207 kWH/Year",
-            "otpion": ["412 KWH/Year", "120 KWH/Year", "420 KWH/Year", "207 KWH/Year"]
+            "question": "Who is said to be responsible for the spread of Jainism in Karnataka?",
+            "answer": "Chandragupta Maurya",
+            "otpion": ["Ajatashatru", "Bimbisara", "Chandragupta Maurya", "Mahapadma Nanda"]
         },
         {
             "id": "da5",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator comes with how many toughened glass shelves?",
-            "answer": "5",
-            "otpion": ["1", "8", "5", "4"]
+            "question": "The origin of Indian music can be traced to which of the following Vedic Samhitas?",
+            "answer": "Samaveda",
+            "otpion": ["Rigveda", "Atharvaveda", "Yajurveda", "Samaveda"]
         }
     ]
 },
 {
     "id": "qz4",
-    "title": "Amazon quiz",
+    "title": "Geography quiz",
     "minutes": 1,
     "imgBanner": "https://thumbs.dreamstime.com/b/main-tenant-le-m-gaphone-jeu-concours-illustration-de-vecteur-courante-144931695.jpg",
     "quiz": [
         {
             "id": "da1",
-            "question": "The Amazon Basics 345L 2 Star Frost-free refrigerator is a _________ door refrigerator.",
-            "answer": "Double",
-            "otpion": ["Single", "Double", "Triple", "Quadruple"]
+            "question": 'Which of the following geographical term related to the "piece of sub-continental land that is surrounded by water"?',
+            "answer": "Island",
+            "otpion": ["Peninsula", "Gulf", "Strait", "Island"]
         },
         {
             "id": "da2",
-            "question": "Which of these features are available in the Amazon Basics 345L 2 Star Frost Free Double Door refrigerator?",
-            "answer": "All of them",
-            "otpion": ["Comfortable", "Affordable", "Useful", "All of them"]
+            "question": "Which of the following water bodies is the home of Lakshadweep?",
+            "answer": "Arabian Sea",
+            "otpion": ["Arabian Sea", "Bay of Bengal", "Indian Ocean", "Atlantic Ocean"]
         },
         {
             "id": "da3",
-            "question": "The vegetable crisper in the Amazon Basics 345L 2 Star Frost-free Double door refrigerator comes with which of these features?",
-            "answer": "All of these",
-            "otpion": ["Good Design", "Portable", "Variety", "All of these"]
+            "question": "Which of the following longitudes is the standard meridian of India?",
+            "answer": "82°30'E",
+            "otpion": ["69°30'E", "75°30'E", "82°30'E", "90°30'E"]
         },
         {
             "id": "da4",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator has an energy consumption of what?",
-            "answer": "207 kWH/Year",
-            "otpion": ["412 KWH/Year", "120 KWH/Year", "420 KWH/Year", "207 KWH/Year"]
+            "question": "In which of the following states is the Wular Lake located?",
+            "answer": "Jammu and Kashmir",
+            "otpion": ["Rajasthan", "Jammu and Kashmir", "Punjab", "Uttar Pradesh"]
         },
         {
             "id": "da5",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator comes with how many toughened glass shelves?",
-            "answer": "5",
-            "otpion": ["1", "8", "5", "4"]
+            "question": "Which of the following reason is famous for the endangered Ridley turtles ?",
+            "answer": "Gahirmatha",
+            "otpion": ["Gahirmatha", "Sagarmatha", "Lakshadweep islands", "Andaman and Nicobar group of islands"]
         }
     ]
 },
 {
     "id": "qz5",
-    "title": "Amazon quiz",
+    "title": "Science quiz",
     "minutes": 1,
     "imgBanner": "https://th.bing.com/th/id/OIP.CUSGCSx0o4fKPcMuyNfW3gHaHu?pid=ImgDet&w=920&h=960&rs=1",
     "quiz": [
         {
             "id": "da1",
-            "question": "The Amazon Basics 345L 2 Star Frost-free refrigerator is a _________ door refrigerator.",
-            "answer": "Double",
-            "otpion": ["Single", "Double", "Triple", "Quadruple"]
+            "question": "The colour of the eye depends upon the pigment present inside?",
+            "answer": "iris",
+            "otpion": ["cornea", "iris", "rods", "cones"]
         },
         {
             "id": "da2",
-            "question": "Which of these features are available in the Amazon Basics 345L 2 Star Frost Free Double Door refrigerator?",
-            "answer": "All of them",
-            "otpion": ["Comfortable", "Affordable", "Useful", "All of them"]
+            "question": "The ability of the eye to see in the dark is due to the production of a purple pigment known as?",
+            "answer": "Retinene",
+            "otpion": ["Carotene", "Rhodopsin", "Iodopsin", "Retinene"]
         },
         {
             "id": "da3",
-            "question": "The vegetable crisper in the Amazon Basics 345L 2 Star Frost-free Double door refrigerator comes with which of these features?",
-            "answer": "All of these",
-            "otpion": ["Good Design", "Portable", "Variety", "All of these"]
+            "question": "Which among the following waves is used for communication by artificial satellites?",
+            "answer": "Microwaves",
+            "otpion": ["The frequency of 101 series", "Radio waves", "A.M", "Microwaves"]
         },
         {
             "id": "da4",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator has an energy consumption of what?",
-            "answer": "207 kWH/Year",
-            "otpion": ["412 KWH/Year", "120 KWH/Year", "420 KWH/Year", "207 KWH/Year"]
+            "question": "When the speed of a car is doubled then what should be the braking force of the car to stop it at the same distance?",
+            "answer": "four times",
+            "otpion": ["two times", "four times", "half", "one fourth"]
         },
         {
             "id": "da5",
-            "question": "The Amazon Basics 305L 3 Star Frost-free refrigerator comes with how many toughened glass shelves?",
-            "answer": "5",
-            "otpion": ["1", "8", "5", "4"]
+            "question": "Cement is made hard by _____",
+            "answer": "hydration and dissociation of water",
+            "otpion": ["dehydration", "hydration and dissociation of water", "Dissociation of water", "Polymerisation"]
         }
     ]
 }
